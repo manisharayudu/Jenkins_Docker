7efe186cfa5e058daa7ff71093a062b7dd3022f9
root <root@ip-10-0-1-99.eu-central-1.compute.internal>
2021-08-05 11:34:58 +0000

    Updated Host address

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var server = app.listen(8081, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://3.67.84.12:8081', host, port);
 });
