5c74cff46c5cbb4b532d720019e6fe6ca82fdf46
root <root@ip-10-0-1-99.eu-central-1.compute.internal>
2021-08-05 11:53:37 +0000

    Updated

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://3.67.84.12:8081', host, port);
+  console.log('Example app listening at http://3.67.84.12:3000', host, port);
 });
