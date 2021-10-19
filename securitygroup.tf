cc854350fefe0e0e7b3f254456bc0e2c80759ecf
Manisha Rayudu <83665305+manisharayudu@users.noreply.github.com>
2021-10-19 15:59:34 +0200

    Create securitygroup.tf

---
+++ securitygroup.tf
@@ -0,0 +1,39 @@
+#Security Group for levelupvpc
+resource "aws_security_group" "allow-levelup-ssh" {
+  vpc_id      = aws_vpc.myproject.id
+  name        = "allow-levelup-ssh"
+  description = "security group that allows ssh connection"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow from Personal CIDR block"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow from Personal CIDR block"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-levelup-ssh"
+  }
+}
